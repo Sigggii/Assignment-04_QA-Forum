c315ace8a42cf0a95b879b789ebb864b060c54f6
Siggi <simeon-holzapfel@gmx.de>
2024-04-25 20:47:24 +0200

    use custom theme

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {CheckboxModule} from "primeng/checkbox";
+import {Button, ButtonModule} from "primeng/button"
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CheckboxModule],
+  imports: [RouterOutlet, CheckboxModule, ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
