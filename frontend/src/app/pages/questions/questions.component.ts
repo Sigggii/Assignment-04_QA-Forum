5bb1713b646b2bfba9210edf38cf2817f12d9cdd
tillgo <till.gohl@gmx.de>
2024-04-27 13:18:40 +0100

    run prettier

--- frontend/src/app/pages/questions/questions.component.ts
+++ frontend/src/app/pages/questions/questions.component.ts
@@ -1,12 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
-  selector: 'app-questions',
-  standalone: true,
-  imports: [],
-  templateUrl: './questions.component.html',
-  styleUrl: './questions.component.css'
+    selector: 'app-questions',
+    standalone: true,
+    imports: [],
+    templateUrl: './questions.component.html',
+    styleUrl: './questions.component.css',
 })
-export class QuestionsComponent {
-
-}
+export class QuestionsComponent {}
