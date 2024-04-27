510e74ce8dc6d0d877695636e342b707274fe070
tillgo <till.gohl@gmx.de>
2024-04-27 11:45:50 +0100

    added logo and fix tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     presets: [require('@spartan-ng/ui-core/hlm-tailwind-preset')],
-    content: ['./src/**/*.{html,ts}'],
+    content: ['./src/**/*.{html,ts}', './lib/ui/**/*.{html,ts}'],
     darkMode: 'class',
     theme: {
         extend: {
